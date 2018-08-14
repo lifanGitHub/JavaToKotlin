54e6b9bd525bf947b0c4dc4d98730c15d6baf0b8
LiFan <494360059@qq.com>
2018-08-14 14:24:30 +0800

    排序

--- src/java_to_kotlin/demo10/In.kt
+++ src/java_to_kotlin/demo10/In.kt
@@ -6,11 +6,11 @@ package java_to_kotlin.demo10
  * isLetter 1和2等效 3不行
  */
 
-fun isLetter1(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
-fun isLetter2(c: Char):Boolean { return c in 'a'..'z' || c in 'A'..'Z' }
-fun isLetter3(c: Char) = { c in 'a'..'z' || c in 'A'..'Z' }
+private fun isLetter1(c: Char) = c in 'a'..'z' || c in 'A'..'Z'
+private fun isLetter2(c: Char):Boolean { return c in 'a'..'z' || c in 'A'..'Z' }
+private fun isLetter3(c: Char) = { c in 'a'..'z' || c in 'A'..'Z' }
 
-fun isNotDigit(c: Char) = c !in '0'..'9'
+private fun isNotDigit(c: Char) = c !in '0'..'9'
 
 fun main(args: Array<String>) {
     println(isLetter3('q'))
--- src/java_to_kotlin/demo10/Sort.kt
+++ src/java_to_kotlin/demo10/Sort.kt
@@ -12,13 +12,16 @@ data class Person(val name: String,
                   val age: Int?)
 
 fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice",22),
-            Person("Bob", age = 29))
+    val persons = listOf(
+            Person("Alice",21),
+            Person("Bob", age = 29),
+            Person("double", age = 29))
 
+    //目测是选择第一个符合条件的
     val oldest = persons.maxBy { it.age ?: 0 }
-    val youngest = persons.maxBy { 1/(it.age ?: -1) } // or minBy Kotlin语法糖真是多
+    // or minBy Kotlin语法糖真是多
+    val youngest = persons.maxBy { 1/(it.age ?: -1) }
+
     println("The oldest is: $oldest")
     println("The oldest is: $youngest")
 }
-
-// The oldest is: Person(name=Bob, age=29)
