afee8215248ef4f189e95a4ac24a1768fada7f8d
LiFan <494360059@qq.com>
2018-08-27 15:51:29 +0800

    关键字 run

--- src/java_to_kotlin/demo13/Run.kt
+++ src/java_to_kotlin/demo13/Run.kt
@@ -5,12 +5,21 @@ package java_to_kotlin.demo13
  * @date 2018/8/27
  */
 fun main(args: Array<String>) {
-    val str = "Aloha"
 
-    //run内部可以定义重复
-    val strRun = run {
-        println(str)
-        str + "_OK"
-    }
-    println(strRun)
+    //Test1
+//    val str = "Aloha"
+//
+//    //run内部可以定义重复（类似于代码块）
+//    val strRun = run {
+//        println(str)
+//        str + "_OK"
+//    }
+//    println(strRun)
+    fun salute() = println("fun!")
+
+    run (::salute)
+    run { salute() }
+
+
+
 }
