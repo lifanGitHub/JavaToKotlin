b279286be21c310cab959ef4db13fad2e3e6e94b
LiFan <494360059@qq.com>
2018-09-06 14:22:32 +0800

    List 常用方法

--- src/java_to_kotlin/demo14/List.kt
+++ src/java_to_kotlin/demo14/List.kt
@@ -3,20 +3,19 @@ package java_to_kotlin.demo14
 /**
  * @author by LiFan
  * @date 2018/8/27
+ * Try Kotlin C_5_3 的3,4一直不理解
  */
 fun main(args: Array<String>) {
     val list = listOf(0, 1, 2, 3, 4, 5)
     list.map { it - 1 }
     list.filter { it > 0 }
-    println(list.any { it != 3 })
+    list.any { it != 3 }
+    list.find { it % 2 == 0}
+    list.groupBy { it.toString() } //转换为Map
 
-    val numbers = mapOf(0 to "zero", 1 to "one")
-    println(numbers.mapValues { it.value.toUpperCase() })
+    val strings = listOf("abc", "def")
+    println(strings.flatMap { it.toList() })
 
-    //是否存在canBeInClub27不允许的成员
-    val people = listOf(Person("Alice", 27), Person("Bob", 31))
-    println(people.all(canBeInClub27))
+
 }
-data class Person(val name: String, val age: Int)
 
-val canBeInClub27 = { p: Person -> p.age <= 27 }
