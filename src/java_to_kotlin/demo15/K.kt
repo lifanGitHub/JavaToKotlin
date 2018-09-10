db32033f252ad3434d3d1a55743d14b83b23c866
LiFan <494360059@qq.com>
2018-09-10 17:10:53 +0800

    操作符重载

---
+++ src/java_to_kotlin/demo15/K.kt
@@ -0,0 +1,16 @@
+package java_to_kotlin.demo15
+
+/**
+ * @author by LiFan
+ * @date 2018/9/10
+ */
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
+    messages.forEach {
+        println("$prefix $it")
+    }
+}
+
+fun main(args: Array<String>) {
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error:")
+}
--- src/java_to_kotlin/demo15/Operate.kt
+++ src/java_to_kotlin/demo15/Operate.kt
@@ -10,8 +10,15 @@ operator fun Point.plus(other: Point): Point {
     return Point(x + other.x, y + other.y)
 }
 
+operator fun Point.unaryMinus(): Point {
+    return Point(-x, -y)
+}
+
 fun main(args: Array<String>) {
     var point = Point(1, 2)
     point += Point(3, 4)
     println(point)
+
+    val p = Point(10, 20)
+    println(-p)
 }
