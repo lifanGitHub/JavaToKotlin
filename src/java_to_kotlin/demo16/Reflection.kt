5d7f2d4b4e35309c9ce0d542b4ce400b04117442
LiFan <494360059@qq.com>
2018-09-17 16:20:50 +0800

    反射

---
+++ src/java_to_kotlin/demo16/Reflection.kt
@@ -0,0 +1,26 @@
+package java_to_kotlin.demo16
+import kotlin.reflect.full.memberProperties
+
+/**
+ * @author by LiFan
+ * @date 2018/9/17
+ */
+
+class Person(val name: String,
+                  val age: Int?,
+                  id :String = "")
+
+fun salute2(a :String = "default") = println("fun!")
+
+fun main(args: Array<String>) {
+    val person = Person("Alice", 29,"1926")
+    val kClass = person.javaClass.kotlin
+    println(kClass.simpleName)
+
+    //未打印出private
+    kClass.memberProperties.forEach { println("${kClass.simpleName}的属性->${it.name}") }
+
+    //方法
+    val fun0 = ::salute2
+    fun0.call("Fun!")
+}
