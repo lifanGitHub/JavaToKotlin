4f4b5fd8cbe11b1f6ff0d8b20feafa23cd3208bd
LiFan <494360059@qq.com>
2018-09-21 14:51:49 +0800

    不含有重复字符的最长子串

---
+++ src/leet_code/P3.kt
@@ -0,0 +1,34 @@
+package leet_code
+
+
+/**
+ * @author by LiFan
+ * @date 2018/9/21
+ * 给定一个字符串，找出不含有重复字符的最长子串的长度。
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 无重复字符的最长子串是 "abc"，其长度为 3。
+ */
+private class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var max = 0
+        for ( i in s.indices){
+            val list: ArrayList<Char> = ArrayList()
+            for ( j in s.indices){
+                if (j < i) continue
+                if (!list.contains(s[j])){
+                    list.add(s[j])
+                }else{
+                    break
+                }
+            }
+            max = if (max >= list.size) max else list.size
+        }
+        return max
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().lengthOfLongestSubstring(" "))
+
+}
