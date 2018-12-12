9419bf557c39d40c309d3968250dde7bb307d006
LiFan <494360059@qq.com>
2018-12-12 14:57:52 +0800

    买卖股票的最佳时机

--- src/java_to_kotlin/demo16/JavaLambda.java
+++ src/java_to_kotlin/demo16/JavaLambda.java
@@ -7,34 +7,40 @@ package java_to_kotlin.demo16;
 
 public class JavaLambda {
     public Interface0 i0;
-    public Interface0x i0x;
     public Interface1 i1;
     public Interface2 i2;
+    public InterfaceX ix;
+//    public InterfaceY i2;
 
 
     public static void main(String[] args) {
         JavaLambda lambda = new JavaLambda();
-        lambda.i0 = () -> System.out.println("hello");
+        lambda.i0 = () -> System.out.println("hello") ;
 //        lambda.i0 = () -> {System.out.println("hello");};  //等效
-        lambda.i0x = () -> "";
+        lambda.i1 = () -> "";
+//        lambda.i1 = () -> { return "" ;};                  //等效
+        lambda.i2 = (s,s2) -> "";
+//        lambda.i2 = (s,s2) -> { return "" ;};              //等效
+
+        //lambda不允许
+//        lambda.ix = () -> System.out.println("hello") ;
     }
 
     public interface Interface0{
         void fun0();
     }
 
-    public interface Interface0x{
-        String fun0x();
+    public interface Interface1 {
+        String fun1();
     }
 
-    public interface Interface1{
-        void fun1();
-        void fun2();
+    public interface Interface2 {
+        String fun2(String s1,String s2);
     }
 
-    public interface Interface2{
-        void fun1();
-        void fun2(String s);
+    public interface InterfaceX {
+        void funX1();
+        String funX2(String s1,String s2);
     }
 
 }
