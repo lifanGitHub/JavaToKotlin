feded50a49020f115661663f086ae36c98f50b72
LiFan <494360059@qq.com>
2018-12-29 15:29:54 +0800

    查找字符串数组中的最长公共前缀

--- src/leet_code/P14.kt
+++ src/leet_code/P14.kt
@@ -3,16 +3,54 @@ package leet_code
 /**
  * @author by LiFan
  * @date 2018/11/6
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
  */
 class Solution {
+    fun commonSize(str1 :String,str2 :String) :Int{
+        val min = if (str1.length >= str2.length) str2.length else str1.length
+        var maxSize = 0
+        for (it in 0 until min){
+            if (str1[it] == str2[it])
+                maxSize++
+            else
+                break
+        }
+        return maxSize
+    }
+
     fun longestCommonPrefix(strs: Array<String>): String {
-        val list = ArrayList<String>()
+        if (strs.isEmpty())
+            return ""
+        if (strs.size == 1)
+            return strs[0]
 
+        val commonSizeList = ArrayList<Int>()
+        for (i1 in strs.indices){
+            val string = strs[i1]
+            var max = Int.MAX_VALUE
+            for (i2 in strs.indices){
+                if (i1 == i2) continue
+                val it = strs[i2]
+                val maxNow = commonSize(it,string)
+                if (maxNow < max)
+                    max = maxNow
+            }
+            commonSizeList.add(max)
+        }
 
-        return ""
+        var maxIndex = 0
+        for (i in commonSizeList.indices){
+            if (commonSizeList[i] > maxIndex){
+                maxIndex = i
+            }
+        }
+
+        return strs[maxIndex].substring(0,commonSizeList[maxIndex])
     }
 
-    fun String.safeGet(n: Int) :String{
-        return if (n < this.length) this[n].toString() else ""
-    }
+}
+
+fun main(args: Array<String>) {
+    println(Solution().longestCommonPrefix(arrayOf("ab","a","cbc")))
 }
