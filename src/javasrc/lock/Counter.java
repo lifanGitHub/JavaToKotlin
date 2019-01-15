d8a95bf5cda6eb045d7e18712c8023d42faef5c3
LiFan <494360059@qq.com>
2019-01-15 11:46:43 +0800

    锁

---
+++ src/javasrc/lock/Counter.java
@@ -0,0 +1,48 @@
+package javasrc.lock;
+
+public class Counter {
+    public static int count = 0;
+
+    public static synchronized void inc() {
+
+        //这里延迟1毫秒，使得结果明显
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+        }
+
+        count++;
+    }
+
+    public static void main(String[] args) {
+
+        //同时启动1000个线程，去进行i++计算，看看实际结果
+
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Counter.inc();
+                }
+            }).start();
+        }
+
+        //这里每次运行的值都有可能不同,可能为1000
+        System.out.println("运行结果:Counter.count=" + Counter.count);
+
+        String s1 = "Aloha";
+        char[] s2 = {'H','e','l','l','o'};
+        change(s1,s2);
+
+    }
+
+    private static void change(String s1, char[] s2){
+        s1 = "world";
+        s2[0] = 'X';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ src/javasrc/lock/WaitAndNotify.java
@@ -0,0 +1,38 @@
+package javasrc.lock;
+
+/**
+ * @author by LiFan
+ * @date 2018/8/10
+ * 线程1 wait()且放下锁-》线程2拿到锁-》线程2通知线程1取消阻塞-》线程2持有obj的锁 所以线程1不能执行继续阻塞
+ * 线程2结束->线程2放弃锁-》线程1拿到锁-》线程1执行
+ */
+
+public class WaitAndNotify {
+    public static void main(String[]args)throws Exception {
+        final Object obj = new Object();
+        new Thread(() -> {
+            synchronized (obj) {
+                try {
+                    obj.wait();
+                    System.out.println("Thread 1 wake up.");
+                } catch (InterruptedException e) {
+                }
+            }
+        }).start();
+
+        Thread.sleep(1000);//We assume lock 1 must start up within 1 sec.
+
+        new Thread(() -> {
+            synchronized (obj) {
+                obj.notifyAll();
+                System.out.println("Thread 2 sent notify.");
+            }
+        }).start();
+    }
+
+
+
+
+
+}
+
