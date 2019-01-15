9e1604264f49b88e6286e8eca28ace167a606ef3
LiFan <494360059@qq.com>
2019-01-15 11:45:46 +0800

    GC测试

---
+++ src/javasrc/menory/Finalize.java
@@ -0,0 +1,50 @@
+package javasrc.menory;
+
+/**
+ * @author by LiFan
+ * @date 2018/8/14
+ */
+
+class Finalize {
+    public static void main(String[] args) {
+        //finalize 只会执行一次
+        A a = new A();
+        a = null;
+        System.gc();
+        A.mine = null;
+        System.gc();
+
+        //finalize 线程优先度低 不保证能完成
+//        B b = new B();
+//        b = null;
+//        System.gc();
+//        System.gc();
+//        System.gc();
+
+        while (true);
+    }
+    static class A {
+        static A mine;
+        private static int n = 0;
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+            System.out.println("finalize执行:" + ++n);
+            mine = this;
+        }
+    }
+
+    static class B {
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println("finalize执行:" + Thread.currentThread().getName());
+            Thread.sleep(5000);
+            System.out.println("finalize执行OK");
+        }
+    }
+
+}
+
+
+
+
