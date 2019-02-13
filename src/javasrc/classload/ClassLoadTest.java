67c88596c10b95a6b86cda3b920d2691a1113a96
LiFan <494360059@qq.com>
2019-02-13 15:48:16 +0800

    类的加载器

---
+++ src/javasrc/classload/ClassLoadTest.java
@@ -0,0 +1,37 @@
+package javasrc.classload;
+
+/**
+ * @author by LiFan
+ * @date 2019/2/13
+ */
+
+class ClassLoadTest {
+    public static void main(String[] args) throws Exception{
+        A a;
+        step();
+
+        print(A.class.getSimpleName());
+        step();
+
+        print(new A[20].length);
+        step();
+
+        print(A.num);
+//        print(A.num2);//num2不行 原理很简单
+        step();
+
+        Class.forName("javasrc.classload.A");
+        step();
+
+
+    }
+
+    static int x = 1;
+    static void step(){
+        System.out.println("step " + x++);
+    }
+    static void print(Object o){
+        System.out.println(o);
+    }
+
+}
