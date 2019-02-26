89f42185b02e8523914bc20273db18cde557bc73
LiFan <494360059@qq.com>
2019-02-26 10:46:09 +0800

    双重检验 枚举 静态内部类单例

---
+++ src/javasrc/singleinstance/Singleton1.java
@@ -0,0 +1,21 @@
+package javasrc.singleinstance;
+
+/**
+ * @author by LiFan
+ * @date 2019/2/21
+ */
+
+public class Singleton1 {
+    private volatile static Singleton1 singleton1;
+    private Singleton1(){}
+    public static Singleton1 getSingleton1() {
+        if (singleton1 == null) {
+            synchronized (Singleton1.class) {
+                if (singleton1 == null) {
+                    singleton1 = new Singleton1();
+                }
+            }
+        }
+        return singleton1;
+    }
+}
