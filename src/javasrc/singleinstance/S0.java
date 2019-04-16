aae664288c0af408f02fdec50ce5bd475661ff4c
LiFan <494360059@qq.com>
2019-04-16 20:18:23 +0800

    lambda的申明和定义

---
+++ src/javasrc/singleinstance/S0.java
@@ -0,0 +1,12 @@
+package javasrc.singleinstance;
+
+/**
+ * @author by LiFan
+ * @date 2019/3/1
+ */
+
+class S0 {
+    public static void father(){
+
+    }
+}
--- src/javasrc/singleinstance/Test.java
+++
@@ -1,9 +0,0 @@
-package javasrc.singleinstance;
-
-/**
- * @author by LiFan
- * @date 2019/2/21
- */
-
-class Test {
-}
