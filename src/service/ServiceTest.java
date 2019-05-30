9e40970d7efac3d12da72ba2cf79c20586fc19d4
LiFan <494360059@qq.com>
2019-05-30 17:10:36 +0800

    null

--- src/service/ServiceTest.java
+++ src/service/ServiceTest.java
@@ -13,17 +13,17 @@ import java.net.Socket;
 
 class ServiceTest {
     public static void main(String[] args) throws Exception{
-        ServerSocket serverSocket = new ServerSocket(8080, 5, InetAddress.getByName("127.0.0.1"));
-        Socket socket = serverSocket.accept(); InputStream is = socket.getInputStream();
-        OutputStream os = socket.getOutputStream();
-
-        byte[] buffer = new byte[2048];
-        StringBuffer info = new StringBuffer(2048);
-        int size = is.read(buffer);
-        for (int i = 0; i < size; i++) info.append((char) buffer[i]);
-        System.out.println(info);
-        String message = "<h1>Hello LiFan</h1>";
-        os.write(message.getBytes());
+//        ServerSocket serverSocket = new ServerSocket(8080, 5, InetAddress.getByName("127.0.0.1"));
+//        Socket socket = serverSocket.accept(); InputStream is = socket.getInputStream();
+//        OutputStream os = socket.getOutputStream();
+//
+//        byte[] buffer = new byte[2048];
+//        StringBuffer info = new StringBuffer(2048);
+//        int size = is.read(buffer);
+//        for (int i = 0; i < size; i++) info.append((char) buffer[i]);
+//        System.out.println(info);
+//        String message = "<h1>Hello LiFan</h1>";
+//        os.write(message.getBytes());
         //测试
 
     }
