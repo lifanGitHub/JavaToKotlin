647df27581a52d5bf617c30e5a211bda555113e5
LiFan <494360059@qq.com>
2019-08-02 10:36:57 +0800

    线程的三种中断方式

--- src/javasrc/Thread/StopThread.java
+++ src/javasrc/Thread/StopThread.java
@@ -11,28 +11,39 @@ package javasrc.Thread;
 
 class StopThread {
     public static void main(String[] args) {
+        DemoThread thread = new DemoThread();
         thread.start();
         sleep(2000);
-//        thread.stop();
-        thread.interrupt();
 
+        //标志位打断
+        thread.setStopFlag();
+        //不能打断非等待状态的线程
+//        thread.interrupt();
+        //弃用不推荐
+//        thread.stop();
     }
 
-    static Thread thread = new Thread(new Runnable() {
+    static class DemoThread extends Thread{
+        private boolean stopFlag;
         @Override
         public void run() {
             try {
-                while (true){
-//                    Thread.sleep(500);
+                while (!stopFlag){
+                    Thread.sleep(500);
                     System.out.println(Thread.interrupted());
                 }
             } catch (Exception e) {
                 System.out.println(e.getClass().getSimpleName());
                 System.out.println(Thread.interrupted());
                 System.out.println(Thread.currentThread().isInterrupted());
+            }finally {
+                //做一些清理
             }
         }
-    });
+        public void setStopFlag() {
+            this.stopFlag = true;
+        }
+    }
 
     private static void sleep(long n){
         try {
@@ -41,11 +52,5 @@ class StopThread {
             e.printStackTrace();
         }
     }
-
-    private static void sleep(){
-        sleep(2000);
-    }
-
-
 
 }
