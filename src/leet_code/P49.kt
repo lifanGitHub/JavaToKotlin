1316b743e67dc0d0a7a54b3e842290da6230b147
lifan <494360059@qq.com>
2020-07-20 16:00:07 +0800

    给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。

--- src/leet_code/P49.kt
+++ src/leet_code/P49.kt
@@ -1,53 +1,50 @@
 package leet_code
 
-import java.util.*
-
 /**
  * Author: LiFan
  * Date: 2020/6/22
- * Description:
+ * Description: 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
  */
 
 class Solution49 {
-
-}
-
-fun String.equalWord(s: String): Boolean {
-    if (s.length != this.length) {
-        return false
-    }
-    val list = s.toCharArray().toMutableList()
-    for (it in this) {
-        if (list.contains(it)) {
-            list.remove(it)
-        } else {
+    fun String.equalWord(s: String): Boolean {
+        if (s.length != this.length) {
             return false
         }
-    }
-    return true
-
-}
-
-fun groupAnagrams(strs: Array<String>): List<List<String>> {
-    val result = mutableListOf<List<String>>()
-    val ignoreList = mutableListOf<Int>()
-
-    for (i in strs.indices) {
-        if (ignoreList.contains(i)) continue
-        val tarStr = strs[i]
-        val list = mutableListOf(tarStr)
-        (i + 1 until strs.size).forEach {
-            if (tarStr.equalWord(strs[it])){
-                list.add(strs[it])
-                ignoreList.add(it)
+        val list = s.toCharArray().toMutableList()
+        for (it in this) {
+            if (list.contains(it)) {
+                list.remove(it)
+            } else {
+                return false
             }
         }
-        result.add(list)
+        return true
     }
-    return result 
+
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val result = mutableListOf<List<String>>()
+        val ignoreList = mutableListOf<Int>()
+
+        for (i in strs.indices) {
+            if (ignoreList.contains(i)) continue
+            val tarStr = strs[i]
+            val list = mutableListOf(tarStr)
+            (i + 1 until strs.size).forEach {
+                if (tarStr.equalWord(strs[it])){
+                    list.add(strs[it])
+                    ignoreList.add(it)
+                }
+            }
+            result.add(list)
+        }
+        return result
+    }
 }
 
+
+
 fun main() {
-    groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+    Solution49().groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
 
 }
